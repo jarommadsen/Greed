02577a8aa03b0afc61163c45e662c1aea2f5c411
Jarom Madsen <jarommadsen@gmail.com>
2016-10-04 00:02:38 -0600

    Started Control implementation

--- Greed/Control.cs
+++ Greed/Control.cs
@@ -7,12 +7,20 @@ namespace Greed
 {
     public static class Control
     {
+        static List<IDice> _diceHand = new List<IDice>();
+        static List<IDice> _diceAside = new List<IDice>();
+        static Player _currentPlayer;
+        static List<Player> _playerList = new List<Player>();
+
         /// <summary>
         /// Initializes a set of 7 dice in hand
         /// </summary>
         static Control() // access modifiers not allowed. constructor runs on first* reference to Control (depending on compiler)
         {
-            throw new System.NotImplementedException();
+            for(int i = 0; i < 7; i++)
+            {
+                _diceHand.Add(new Dice6());
+            }
         }
 
         /// <summary>
@@ -22,7 +30,7 @@ namespace Greed
         {
             get
             {
-                throw new System.NotImplementedException();
+                return _diceAside;
             }
 
             private set
@@ -37,7 +45,7 @@ namespace Greed
         {
             get
             {
-                throw new System.NotImplementedException();
+                return _diceHand;
             }
 
             private set
@@ -52,11 +60,12 @@ namespace Greed
         {
             get
             {
-                throw new System.NotImplementedException();
+                return _playerList;
             }
 
             private set
             {
+                throw new NotImplementedException();
             }
         }
 
@@ -67,7 +76,7 @@ namespace Greed
         {
             get
             {
-                throw new System.NotImplementedException();
+                return _currentPlayer;
             }
         }
 
@@ -77,7 +86,14 @@ namespace Greed
         /// <param name="die">die to set aside</param>
         public static void SetAside(IDice die)
         {
-            throw new System.NotImplementedException();
+            if (_diceHand.Contains(die))
+            {
+                _diceHand.Remove((Dice6)die);
+                _diceAside.Add((Dice6)die);
+            }else
+            {
+                throw new Exception("Specified dice did not exist in hand");
+            }
         }
 
         /// <summary>
@@ -86,7 +102,15 @@ namespace Greed
         /// <param name="die">die to take in hand</param>
         public static void SetInHand(IDice die)
         {
-            throw new System.NotImplementedException();
+            if (_diceAside.Contains(die))
+            {
+                _diceAside.Remove((Dice6)die);
+                _diceHand.Add((Dice6)die);
+            }
+            else
+            {
+                throw new Exception("Specified dice did not exist in set aside dice");
+            }
         }
 
         /// <summary>
@@ -95,13 +119,19 @@ namespace Greed
         /// <param name="player">player to add to game</param>
         public static void AddPlayer(Player player)
         {
-            throw new System.NotImplementedException();
+            _playerList.Add(player);
         }
 
         /// <param name="player">player to remove from game</param>
         public static void RemovePlayer(Player player)
         {
-            throw new System.NotImplementedException();
+            if (_playerList.Contains(player))
+            {
+                _playerList.Remove(player);
+            }else
+            {
+                throw new Exception("'player' does not exist in list of players");
+            }
         }
 
         /// <summary>
@@ -110,7 +140,7 @@ namespace Greed
         /// <param name="diceList">group of dice to check</param>
         public static bool DiceGroupHasPoints(List<IDice> diceList)
         {
-            throw new System.NotImplementedException();
+            return GetDicePoints(diceList) > 0;
         }
 
         /// <summary>
@@ -128,17 +158,73 @@ namespace Greed
         /// <param name="diceList">group of dice to check</param>
         public static int GetDicePoints(List<IDice> diceList)
         {
-            throw new System.NotImplementedException();
+            //1's are 100
+            //5's are worth 50
+            //set's of 3 are worth 100x their face besides 1's and 5's
+            //each additional die doubles their value (4 is double, 5 is double of 4, etc.);
+            int total = 0;//the total points
+            int[] numbers = GetDiceCountArray(diceList);
+            int val;
+            for (int i = 0; i < 6; i++)
+            {
+                if (numbers[i] > 0)
+                {
+                    switch (i + 1)
+                    {
+                        case 1:
+                            val = 100;
+                            break;
+                        case 5:
+                            val = 50;
+                            break;
+                        default:
+                            val = (i + 1) * 10;
+                            break;
+                    }
+                    if (numbers[i] >= 3)
+                    {
+                        val *= 10;
+                        if (numbers[i] > 3)
+                        {
+                            val *= (int)Math.Pow(2, numbers[i] - 3);
+                        }
+                    }
+                    else
+                    {
+                        if (i != 0 && i != 4)
+                        {
+                            val = 0;
+                        }
+                    }
+                    total += val;
+                }
+            }
+            return total;
+        }
+
+        private static int[] GetDiceCountArray(List<IDice> diceList)
+        {
+            int[] numbers = new int[6];//how many of each number there is
+            foreach (IDice element in diceList)
+            {
+                numbers[element.Face - 1]++;
+            }
+
+            return numbers;
         }
 
         /// <summary>
-        /// true if the specified die has a point value greater than zero in the group of dice (also true if part of a set of 3 or more in the group of dice).  False if die does not belong to the provided group
+        /// true if the specified die has a point value greater than zero in the
+        /// group of dice (also true if part of a set of 3 or more in the group of dice).  
+        /// False if die does not belong to the provided group
         /// </summary>
         /// <param name="die">die to check</param>
         /// <param name="group">group of dice holding the specified die</param>
         public static bool DieHasWorth(IDice die, List<IDice> group)
         {
-            throw new System.NotImplementedException();
+            if (die.Face == 1 || die.Face == 5) return true;
+            int[] numbers = GetDiceCountArray(group);
+            return numbers[die.Face - 1] >= 3;
         }
 
         /// <summary>
---
+++ GreedTests/ControlTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Greed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greed.Tests
+{
+    [TestClass()]
+    public class ControlTests
+    {
+        [TestMethod()]
+        public void DiceGroupHasPointsTest()
+        {
+            List<IDice> list = new List<IDice>();
+            Dice6 die = new Dice6();
+            die.Face = 3;
+            list.Add(die);
+
+            die = new Dice6();
+            die.Face = 3;
+            list.Add(die);
+
+            die = new Dice6();
+            die.Face = 2;
+            list.Add(die);
+            //Assert.IsFalse(Control.DiceGroupHasPoints(list));
+
+            die = new Dice6();
+            die.Face = 3;
+            list.Add(die);
+            Assert.IsTrue(Control.DiceGroupHasPoints(list));
+
+        }
+
+        [TestMethod()]
+        public void GetDicePointsTest()
+        {
+            List<IDice> list = new List<IDice>();
+            Dice6 die = new Dice6();
+            die.Face = 1;
+            list.Add(die);
+
+            die = new Dice6();
+            die.Face = 5;
+            list.Add(die);
+
+            die = new Dice6();
+            die.Face = 5;
+            list.Add(die);
+
+            die = new Dice6();
+            die.Face = 5;
+            list.Add(die);
+
+            int total = Control.GetDicePoints(list);
+            Assert.IsTrue(total == 600,"Total was: "+total);
+        }
+    }
+}
